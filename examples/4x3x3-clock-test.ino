b95b25e3c3df844f419c2ea919cf872642f56b13
Marcin Saj <marcin@nixietester.com>
2025-03-17 11:22:24 +0100

    Update 4x3x3-clock-test.ino

--- examples/4x3x3-clock-test.ino
+++ examples/4x3x3-clock-test.ino
@@ -1,1 +1,49 @@
+#include <FlipDisc.h>
 
+// Pin declaration for Arduino Nano Every and PSPS module
+#define EN_PIN    PIN_PB2  // Arduino pin 10. Start & End SPI transfer data
+#define CH_PIN    PIN_PB0  // Arduino pin 8. Charging PSPS module - turn ON/OFF
+#define PL_PIN    PIN_PB1  // Arduino pin 9. Release the current pulse - turn ON/OFF
+
+// LDBG LED
+// Used to indicate potential hardware issues during the boot process
+#define LDBG_PIN  PIN_PC2  // Arduino pin A2
+
+
+
+void setup()
+{
+  Flip.Pin(EN_PIN, CH_PIN, PL_PIN);
+ 
+  /* Initialization function of the displays */
+  Flip.Init(D4X3X3);
+
+  pinMode(LDBG_PIN, OUTPUT);
+  digitalWrite(LDBG_PIN, LOW);
+
+
+  delay(1000);
+  digitalWrite(LDBG_PIN, HIGH);
+  delay(1000);
+      
+  Flip.Delay(100);
+}
+
+void loop()
+{
+  Flip.Clear();
+  delay(1000);
+  digitalWrite(LDBG_PIN, LOW);
+
+  Flip.All();
+  delay(1000);
+  digitalWrite(LDBG_PIN, HIGH);
+
+  Flip.Clear();
+  delay(1000);
+  digitalWrite(LDBG_PIN, LOW);
+
+  Flip.All();
+  delay(1000);
+  digitalWrite(LDBG_PIN, HIGH);
+}
